b47cba1a43516eebff780c3aea2617fabb4c13aa
VitaliiKalinbet <bosssvit@gmail.com>
2020-02-01 19:17:38 +0200

    change index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const startServer = require('./src/server');
-const connectToDB = require('./src/domain/db/connect-db');
-const { port, databaseUrl } = require('./config');
-const {schedule} = require('./src/controller');
+const startServer = require("./src/server");
+const connectToDB = require("./src/domain/db/connect-db");
+const { port, databaseUrl } = require("./config");
+const { schedule } = require("./src/controller");
 
-
-console.log('databaseUrl :', databaseUrl);
+console.log("databaseUrl :", databaseUrl);
 
 startServer(process.env.PORT || port);
 connectToDB(databaseUrl);
-// schedule();
+schedule();
